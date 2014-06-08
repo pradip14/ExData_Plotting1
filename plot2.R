8429b09b16f9b72e1cbacaf3ab7dd334107f90a1
pradip14 <pradipsaud@hotmail.com>
2014-06-08 12:18:49 -0500

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,4 @@
+#plot 2
+png(filename = "plot2.png", width = 480, height = 480, units = "px")
+plot(projl$DateTime, proj1$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
